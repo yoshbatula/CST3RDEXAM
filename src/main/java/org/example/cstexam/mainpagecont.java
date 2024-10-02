e40fe6ef7cc20bf83398dd0505882b2c6409512b
CCE107 <yoshbatula2@gmail.com>
2024-10-02 11:23:34 +0800

    MAINPAGE FUNCTIONALITY

--- src/main/java/org/example/cstexam/mainpagecont.java
+++ src/main/java/org/example/cstexam/mainpagecont.java
@@ -1,4 +1,30 @@
 package org.example.cstexam;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class mainpagecont {
+
+    @FXML
+    private Button startBTN;
+
+    public void start(ActionEvent event) throws IOException {
+        if(event.getSource() == startBTN) {
+
+            Stage window = (Stage) startBTN.getScene().getWindow();
+            window.close();
+
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
 }
