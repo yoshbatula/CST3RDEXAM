080b29a855f577be381976e3ea8e6fd38ebf06fa
CCE107 <yoshbatula2@gmail.com>
2024-10-02 11:35:07 +0800

    NAAY ANIMATION

--- src/main/java/org/example/cstexam/mainpagecont.java
+++ src/main/java/org/example/cstexam/mainpagecont.java
@@ -1,15 +1,22 @@
 package org.example.cstexam;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class mainpagecont {
+public class mainpagecont implements Initializable {
 
     @FXML
     private Button startBTN;
@@ -26,5 +33,17 @@ public class mainpagecont {
             stage.setScene(scene);
             stage.show();
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        FadeTransition fade = new FadeTransition();
+        fade.setNode(startBTN);
+        fade.setDuration(Duration.millis(2000));
+        fade.setCycleCount(FadeTransition.INDEFINITE);
+        fade.setInterpolator(Interpolator.LINEAR);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.play();
     }
 }
